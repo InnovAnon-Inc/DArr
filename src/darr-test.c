2b508720a79a24387561b41d723ad3c8cba30fbb
Default User <laurence.a.maddox@gmail.com>
2017-09-27 02:02:21 -0500

    backup

--- src/darr-test.c
+++ src/darr-test.c
@@ -21,23 +21,24 @@
 __attribute__ ((nothrow, warn_unused_result))
 int main (void) {
    darr_t darr;
-   double factor = 2;
+   double dfactor = 2;
+   size_t sfactor = 2;
    time_t t;
    int num;
    int nums[100];
    int tmps[ARRSZ (nums)];
    size_t k;
-   darr_resize_cb_t cbs[3];
+   /*darr_resize_cb_t cbs[3];*/
 
    t = time (NULL);
    srand ((unsigned int) t);
 
-   cbs[0] = darr_resize_exact;
+   /*cbs[0] = darr_resize_exact;
    cbs[1] = darr_resize_linear;
-   cbs[2] = darr_resize_geometric;
+   cbs[2] = darr_resize_geometric;*/
 
    error_check (init_darr (&darr, sizeof (int),
-      darr_resize_geometric, &factor) != 0) {
+      darr_resize_geometric, &dfactor) != 0) {
       puts ("error -1"); fflush (stdout);
       return -1;
    }
@@ -163,29 +164,117 @@ int main (void) {
    }
 
    /*
+   free_darr (&darr);
+
+   error_check (init_darr (&darr, sizeof (int),
+      darr_resize_geometric, &factor) != 0) {
+      puts ("error -5"); fflush (stdout);
+      free_darr (&darr);
+      return -5;
+   }
+   */
+
    switch (rand () % 4) {
    case 0:
-      insert_rear_darr ();
+      num = rand ();
+      error_check (insert_rear_darr (&darr, &num) != 0) {
+         puts ("error -10"); fflush (stdout);
+         free_darr (&darr);
+         return -10;
+      }
       break;
    case 1:
-      inserts_rear_darr ();
+      num = rand () % ARRSZ (nums);
+      for (k = 0; k != num; k++)
+         nums[k] = rand ();
+      error_check (inserts_rear_darr (&darr, nums, num) != 0) {
+         puts ("error -11"); fflush (stdout);
+         free_darr (&darr);
+         return -11;
+      }
       break;
    case 2:
-      remove_rear_darr ();
+      if (darr.n == 0) break;
+      remove_rear_darr (&darr, &num);
       break;
    case 3:
-      removes_rear_darr ();
+      if (darr.n == 0) break;
+      num = rand () % darr.n;
+      removes_rear_darr (&darr, nums, num);
       break;
    case 4:
-      darr_trim_cap ();
+      if (darr.n == 0) k = 0;
+      else k = rand () % darr.n;
+      num = rand ();
+      error_check (insert_front_darr (&darr, k, &num) != 0) {
+         puts ("error -12"); fflush (stdout);
+         free_darr (&darr);
+         return -12;
+      }
       break;
    case 5:
-      darr.resizecb = cbs[?];
+      num = rand () % ARRSZ (nums);
+      for (k = 0; k != num; k++)
+         nums[k] = rand ();
+      if (darr.n == 0) k = 0;
+      else k = rand () % darr.n;
+      error_check (inserts_front_darr (&darr, k, nums, num) != 0) {
+         puts ("error -13"); fflush (stdout);
+         free_darr (&darr);
+         return -13;
+      }
+      break;
+   case 6:
+      if (darr.n == 0) break;
+      k = rand () % darr.n;
+      remove_front_darr (&darr, k, &num);
+      break;
+   case 7:
+      if (darr.n == 0) break;
+      k = rand () % darr.n;
+      num = k + rand () % (darr.n - k);
+      error_check (removes_front_darr (&darr, k, nums, num) != 0) {
+         puts ("error -14"); fflush (stdout);
+         free_darr (&darr);
+         return -14;
+      }
+      break;
+   case 8:
+      if (darr.n == 0); break;
+      num = rand () % darr.n;
+      error_check (trim_cap_darr (&darr, num) != 0) {
+         puts ("error -15"); fflush (stdout);
+         free_darr (&darr);
+         return -15;
+      }
+      break;
+   case 9:
+      num = rand () % 3;
+      switch (num) {
+      case 0:
+         darr.resizecb = darr_resize_exact;
+         darr.cbargs   = NULL;
+         break;
+      case 1:
+         darr.resizecb = darr_resize_linear;
+         sfactor = (size_t) (rand () + 1);
+         darr.cbargs = &sfactor;
+         break;
+      case 2:
+         darr.resizecb = darr_resize_geometric;
+         k = rand ();
+         do num = rand ();
+         while (num == 0);
+         dfactor = (double) k / (double) num;
+         darr.cbargs = &dfactor;
+         break;
+      default: __builtin_unreachable ();
+      }
+      darr.resizecb = cbs[num];
       darr.cbargs   = cbargs[?]; // TODO use random factor
       break;
    default: __builtin_unreachable ();
    }
-   */
 
    free_darr (&darr);
 
