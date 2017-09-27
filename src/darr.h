991bedbd07c1841afcb6469f75c8906573da99c2
Default User <laurence.a.maddox@gmail.com>
2017-09-26 20:42:32 -0500

    backup

--- src/darr.h
+++ src/darr.h
@@ -26,11 +26,11 @@ typedef struct {
 } darr_t;
 
 int init_darr (darr_t *restrict darr, size_t esz,
-   darr_resize_cb_t resizecb)
+   darr_resize_cb_t resizecb, void *restrict cbargs)
 __attribute__ ((nonnull (1, 3), nothrow, warn_unused_result)) ;
 
 int init_darr2 (darr_t *restrict darr, size_t maxn, size_t esz,
-   darr_resize_cb_t resizecb)
+   darr_resize_cb_t resizecb, void *restrict cbargs)
 __attribute__ ((leaf, nonnull (1, 4), nothrow, warn_unused_result)) ;
 
 int ensure_cap_darr (darr_t *restrict darr, size_t n)
